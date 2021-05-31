668481b694a889a9b335581a7d043da5d69fc203
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-31 18:07:41 +0530

    Refactor:UC2-CalculateWage

--- EmpWageCompution/EmployeeWage.cs
+++ EmpWageCompution/EmployeeWage.cs
@@ -4,17 +4,24 @@ using System.Text;
 
 namespace EmpWageCompution
 {
-    class EmployeeWage
+    public class EmployeeWage
     {
-        int IS_PRESENT = 1;
-        public void Attendence()
+        const int IS_PRESENT = 1;
+        int EMPHRS = 8;
+        int EMP_RATE_PER_HRS = 20;
+        public void Attendance()
         {
+            //Employee attendance
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_PRESENT)
-                Console.WriteLine("Employee is Present");
+            {
+                //calculate wage
+                int Wage = EMPHRS * EMP_RATE_PER_HRS;
+                Console.WriteLine("Employee is present wage :  " + Wage);
+            }
             else
-                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Employee is absent wage :" + 0);
         }
     }
 }
