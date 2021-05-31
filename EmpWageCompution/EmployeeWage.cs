6530333f0279f3f4e13dbbccec98ccb26be30f0c
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-31 18:02:47 +0530

    Refactor:EmployeeWage

---
+++ EmpWageCompution/EmployeeWage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageCompution
+{
+    class EmployeeWage
+    {
+        int IS_PRESENT = 1;
+        public void Attendence()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
--- EmpWageCompution/Program.cs
+++ EmpWageCompution/Program.cs
@@ -7,6 +7,8 @@ namespace EmpWageCompution
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+            EmployeeWage employeewage = new EmployeeWage();
+            employeewage.Attendence();
         }
     }
 }
