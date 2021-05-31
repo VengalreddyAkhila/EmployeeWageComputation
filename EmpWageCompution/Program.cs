10a28c0f53ec6a8ad4020e6c482be61f51162d7d
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-31 17:54:38 +0530

    Added:EmployeeWage

---
+++ EmpWageCompution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageCompution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+        }
+    }
+}
