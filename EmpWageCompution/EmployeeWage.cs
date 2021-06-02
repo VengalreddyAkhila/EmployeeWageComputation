da8dd8508cef53c229d998a49bd9fd90117d0dca
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-02 21:26:02 +0530

    Refactor:employeewage

--- EmpWageCompution/EmployeeWage.cs
+++ EmpWageCompution/EmployeeWage.cs
@@ -23,14 +23,6 @@ namespace EmpWageCompution
         int EmpHrs = 0;
         int EMP_RATE_PER_HRS = 20;
         int NUMBER_OF_WORKING_DAYS = 20;
-<<<<<<< HEAD
-        int totalempwage = 0;
-        public void Calculate(int EMP_RATE_PER_HRS, int NUMBER_OF_WORKING_DAYS)
-        {
-            this.EMP_RATE_PER_HRS = EMP_RATE_PER_HRS;
-            this.NUMBER_OF_WORKING_DAYS = NUMBER_OF_WORKING_DAYS;
-            for (int day = 0; day < this.NUMBER_OF_WORKING_DAYS; day++)
-=======
         int MAX_HRS_IN_MONTH = 100;
         int TotalEmpHrs = 0;
         int TotalWorkingDays = 0;
@@ -40,8 +32,8 @@ namespace EmpWageCompution
             this.NUMBER_OF_WORKING_DAYS = NUMBER_OF_WORKING_DAYS;
             this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
             while (EmpHrs <= this.MAX_HRS_IN_MONTH && TotalWorkingDays <= this.NUMBER_OF_WORKING_DAYS)
->>>>>>> UC6-Maxdays
             {
+                TotalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -56,13 +48,6 @@ namespace EmpWageCompution
                         EmpHrs = 0;
                         break;
                 }
-<<<<<<< HEAD
-                int Wage = EmpHrs * this.EMP_RATE_PER_HRS;
-                totalempwage =  totalempwage + Wage;
-                Console.WriteLine("wage:" + Wage);
-            }
-                Console.WriteLine(" totalempwage: " + totalempwage);
-=======
                 TotalEmpHrs = TotalEmpHrs + EmpHrs;
                 Console.WriteLine("totalworkingdays" + TotalWorkingDays);
                 Console.WriteLine("emphrs" + EmpHrs);
@@ -70,7 +55,6 @@ namespace EmpWageCompution
             int Wage = EmpHrs * this.EMP_RATE_PER_HRS;
             Console.WriteLine("wage:" + Wage);
 
->>>>>>> UC6-Maxdays
         }
     }
     
--- EmpWageCompution/Program.cs
+++ EmpWageCompution/Program.cs
@@ -10,11 +10,7 @@ namespace EmpWageCompution
             // Employee employee = new Employee();
             // employee.Attendance();
             EmployeeWage empwage = new EmployeeWage();
-<<<<<<< HEAD
-            empwage.Calculate(20,20);
-=======
             empwage.Calculate(20,20, 100);
->>>>>>> UC6-Maxdays
         }
     }
 }
