317e86bea924ffd6be60b736de182075a06b2e2c
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-02 20:12:54 +0530

    Refactor:EmployeeWage

--- EmpWageCompution/EmployeeWage.cs
+++ EmpWageCompution/EmployeeWage.cs
@@ -4,28 +4,6 @@ using System.Text;
 
 namespace EmpWageCompution
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-     class EmployeeWage
-    { 
-         //constants
-        public const int IS_FULL_TIME = 1;
-        public  const int IS_PART_TIME = 2;
-        public string Company;
-        public int EMP_RATE_PER_HOUR;
-        public int NUMBER_OF_WORKING_DAYS;
-        public int MAX_HRS_IN_MONTH;
-        public int Wage;
-        public EmployeeWage(string Company, int EMP_RATE_PER_HOUR, int NUMBER_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH)
-        {
-            this.Company = Company;
-            this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
-            this.NUMBER_OF_WORKING_DAYS = NUMBER_OF_WORKING_DAYS;
-            this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
-=======
-=======
->>>>>>> UC2-CalculateWage
     //using oops concept  Interface
     interface  IAttendanceCalculation
     {
@@ -33,20 +11,6 @@ namespace EmpWageCompution
     }
     class Employee : IAttendanceCalculation
     {
-<<<<<<< HEAD
-        public  void Attendance()
-        {
-            int IS_PRESENT = 1;
-=======
-=======
-    //using oops concept  Interface
-    interface  IAttendanceCalculation
-    {
-        public void Attendance();
-    }
-    class Employee : IAttendanceCalculation
-    {
->>>>>>> UC3-PartTime
         public void Attendance()
         {
            Console.WriteLine("Employee Attendance");
@@ -56,83 +20,28 @@ namespace EmpWageCompution
     { 
         public void Calculate()
         {
-<<<<<<< HEAD
-            const int IS_PRESENT = 1;
-            int EMPHRS = 8;
-            int EMP_RATE_PER_HRS = 20;
->>>>>>> UC2-CalculateWage
-=======
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             int EmpHrs = 8;
             int EMP_RATE_PER_HRS = 20;
->>>>>>> UC3-PartTime
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_PRESENT)
-                Console.WriteLine("Employee is Present");
-            else
-                Console.WriteLine("Employee is Absent");
->>>>>>> UC1-Attendance
-        }
-
-            public void ComputeEmpWage()
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_FULL_TIME)
             {
-<<<<<<< HEAD
-                //variables
-                int EmpHrs = 0;
-                int TotalWorkingDays = 0;
-                int TotalEmpHrs = 0;
-                //calculating Hrs per month
-                while (EmpHrs <= this.MAX_HRS_IN_MONTH && TotalWorkingDays <= this.NUMBER_OF_WORKING_DAYS)
-                {
-                    TotalWorkingDays++;
-                    Random random = new Random();
-                    int empCheck = random.Next(0, 3);
-                    //using switch case
-                    switch (empCheck)
-                    {
-                        case IS_FULL_TIME:
-                            EmpHrs = 8;
-                            break;
-                        case IS_PART_TIME:
-                            EmpHrs = 4;
-                            break;
-                        default:
-                            EmpHrs = 0;
-                            break;
-                    }
-                    //calculating total hrs
-                    TotalEmpHrs = TotalEmpHrs + EmpHrs;
-                    Console.WriteLine("totalworkingdays" + TotalWorkingDays);
-                    Console.WriteLine("emphrs" + EmpHrs);
-                }
-                Wage = EmpHrs * this.EMP_RATE_PER_HOUR;
-                Console.WriteLine("wage:" + Company + "is" +Wage );
+                EmpHrs = 8;
             }
-        public string Value()
-        {
-            return "total employee wage for company" + this.Company + " is" + this.Wage;
-=======
-                int Wage = EMPHRS * EMP_RATE_PER_HRS;
-                Console.WriteLine("Employee is present wage :  " + Wage);
+            else if (empCheck == IS_PART_TIME)
+            {
+                EmpHrs = 4;
             }
-
             else
             {
-                Console.WriteLine("Employee is absent wage :" + 0);
+                EmpHrs = 0;
             }
-<<<<<<< HEAD
->>>>>>> UC2-CalculateWage
-        }
-                   
-     }
-=======
             int Wage = EmpHrs * EMP_RATE_PER_HRS;
             Console.WriteLine("wage:" + Wage);
         }
     }
->>>>>>> UC3-PartTime
     
 }
   
--- EmpWageCompution/Program.cs
+++ EmpWageCompution/Program.cs
@@ -7,30 +7,10 @@ namespace EmpWageCompution
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            EmployeeWage Max= new EmployeeWage("max", 20 ,3,10);
-            Max.ComputeEmpWage();
-            Console.WriteLine(Max.Value());
-            EmployeeWage Reliance = new EmployeeWage("Reliance", 10, 2, 10);
-            Reliance.ComputeEmpWage();
-            Console.WriteLine(Reliance.Value());
-=======
-            Employee employee = new Employee();
-            employee.Attendance();
->>>>>>> UC1-Attendance
-=======
-=======
->>>>>>> UC3-PartTime
             // Employee employee = new Employee();
             // employee.Attendance();
             EmployeeWage empwage = new EmployeeWage();
             empwage.Calculate();
-<<<<<<< HEAD
->>>>>>> UC2-CalculateWage
-=======
->>>>>>> UC3-PartTime
         }
     }
 }
