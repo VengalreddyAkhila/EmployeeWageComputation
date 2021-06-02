59ed6ec26545ea7fbf750da3d8bcbaaa7d342078
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-02 21:09:03 +0530

    Refactor:UC5-WagesForMonth

--- EmpWageCompution/EmployeeWage.cs
+++ EmpWageCompution/EmployeeWage.cs
@@ -6,42 +6,6 @@ namespace EmpWageCompution
 {
     //using oops concept  Interface
     interface  IAttendanceCalculation
-<<<<<<< HEAD
-    {
-        public void Attendance();
-    }
-    class Employee : IAttendanceCalculation
-    {
-        public void Attendance()
-        {
-           Console.WriteLine("Employee Attendance");
-        }
-    }
-    class EmployeeWage :Employee , IAttendanceCalculation 
-    { 
-        public void Calculate()
-        {
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-            int EmpHrs = 0;
-            int EMP_RATE_PER_HRS = 20;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
-            {
-                case IS_FULL_TIME:
-                    EmpHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    EmpHrs = 4;
-                    break;
-                default:
-                    EmpHrs = 0;
-                    break;
-            }
-            int Wage = EmpHrs * EMP_RATE_PER_HRS;
-            Console.WriteLine("wage:" + Wage);
-=======
     {
         public void Attendance();
     }
@@ -85,7 +49,6 @@ namespace EmpWageCompution
                 Console.WriteLine("wage:" + Wage);
             }
                 Console.WriteLine(" totalempwage: " + totalempwage);
->>>>>>> UC5-WagesForMonth
         }
     }
     
--- EmpWageCompution/Program.cs
+++ EmpWageCompution/Program.cs
@@ -10,11 +10,7 @@ namespace EmpWageCompution
             // Employee employee = new Employee();
             // employee.Attendance();
             EmployeeWage empwage = new EmployeeWage();
-<<<<<<< HEAD
-            empwage.Calculate();
-=======
             empwage.Calculate(20,20);
->>>>>>> UC5-WagesForMonth
         }
     }
 }
