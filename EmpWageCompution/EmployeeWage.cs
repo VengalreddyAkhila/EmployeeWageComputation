5fa4fc264918d7fe1a5d7e3f10c37cdbc8a14b9a
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-02 21:22:05 +0530

    Refactor:UC6-Maxdays

--- EmpWageCompution/EmployeeWage.cs
+++ EmpWageCompution/EmployeeWage.cs
@@ -4,20 +4,34 @@ using System.Text;
 
 namespace EmpWageCompution
 {
-    public class EmployeeWage
+    //using oops concept  Interface
+    interface  IAttendanceCalculation
+    {
+        public void Attendance();
+    }
+    class Employee : IAttendanceCalculation
+    {
+        public void Attendance()
+        {
+           Console.WriteLine("Employee Attendance");
+        }
+    }
+    class EmployeeWage :Employee , IAttendanceCalculation 
     {
         const int IS_FULL_TIME = 1;
         const int IS_PART_TIME = 2;
-        const int EMP_RATE_PER_HOUR = 20;
-        const int NUMBER_OF_WORKING_DAYS = 20;
-        const int MAX_HRS_IN_MONTH = 100;
         int EmpHrs = 0;
-        int TotalWorkingDays = 0;
+        int EMP_RATE_PER_HRS = 20;
+        int NUMBER_OF_WORKING_DAYS = 20;
+        int MAX_HRS_IN_MONTH = 100;
         int TotalEmpHrs = 0;
-        //Total working Hrs per month
-        public void Employee()
+        int TotalWorkingDays = 0;
+        public void Calculate(int EMP_RATE_PER_HRS, int NUMBER_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH)
         {
-            while (EmpHrs <= MAX_HRS_IN_MONTH && TotalWorkingDays <= NUMBER_OF_WORKING_DAYS)
+            this.EMP_RATE_PER_HRS = EMP_RATE_PER_HRS;
+            this.NUMBER_OF_WORKING_DAYS = NUMBER_OF_WORKING_DAYS;
+            this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
+            while (EmpHrs <= this.MAX_HRS_IN_MONTH && TotalWorkingDays <= this.NUMBER_OF_WORKING_DAYS)
             {
                 TotalWorkingDays++;
                 Random random = new Random();
@@ -36,11 +50,18 @@ namespace EmpWageCompution
                 }
                 TotalEmpHrs = TotalEmpHrs + EmpHrs;
                 Console.WriteLine("totalworkingdays" + TotalWorkingDays);
-                Console.WriteLine ("emphrs" + EmpHrs);
+                Console.WriteLine("emphrs" + EmpHrs);
             }
-                int Wage = EmpHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("wage:" + Wage);
-                   
+            int Wage = EmpHrs * this.EMP_RATE_PER_HRS;
+            Console.WriteLine("wage:" + Wage);
+
         }
     }
+    
+    
 }
+  
+
+        
+    
+
