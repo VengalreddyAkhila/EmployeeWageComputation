9d11160b94cbf4c36fb614ffbdd2831e5d30fecb
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-02 20:19:08 +0530

    Refactor:UC4-PartTime_Wage_Cases

--- EmpWageCompution/EmployeeWage.cs
+++ EmpWageCompution/EmployeeWage.cs
@@ -22,26 +22,27 @@ namespace EmpWageCompution
         {
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
-            int EmpHrs = 8;
+            int EmpHrs = 0;
             int EMP_RATE_PER_HRS = 20;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                EmpHrs = 8;
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                EmpHrs = 4;
-            }
-            else
-            {
-                EmpHrs = 0;
+                case IS_FULL_TIME:
+                    EmpHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    EmpHrs = 4;
+                    break;
+                default:
+                    EmpHrs = 0;
+                    break;
             }
             int Wage = EmpHrs * EMP_RATE_PER_HRS;
             Console.WriteLine("wage:" + Wage);
         }
     }
+    
     
 }
   
